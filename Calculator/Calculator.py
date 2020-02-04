2d1d95ed655082f8c4a82e63bcd9e866487e5071
kn285 <55518570+kn285@users.noreply.github.com>
2020-02-04 13:45:40 -0500

    Feature: Added MathOperations Folder and subfiles

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,22 +1,14 @@
+from MathOperations.addition import Addition
 class Calculator:
     def __init__(self):
         pass
 
-    def addition(self, a, b):
-        return float(a) + float(b)
+    def Sum(self, a, b):
+        return Addition.sum(a,b)
 
-    def subtraction(self, a, b):
-        return float(a) - float(b)
+    def Subtraction(self, a, b):
+        return Subtraction.difference(a,b)
 
-    def multiplication(self, a, b):
-        return float(a) * float(b)
 
-    def division(self, a, b):
-        return float(b) / float(a)
 
-    def squareroot(self, a):
-        return float(a ** 0.5)
-
-    def square(self, a):
-        return float(a) * float(a)
 
--- MathOperations/addition.py
+++ MathOperations/addition.py
@@ -1,5 +1,5 @@
-class static Addition:
-
-    static def sum(self, a, b):
-        return a + b
+class Addition:
+    @staticmethod
+    def Sum(augend, addend):
+        return (augend + addend)
     
